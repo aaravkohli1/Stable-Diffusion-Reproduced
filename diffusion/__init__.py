2a8bb9c74ff5270ead25ec965d213b79c3bc001f
kian <kian.benner@mail.utoronto.ca>
2026-02-15 15:17:39 -0500

    Complete DDPM sampler

--- diffusion/__init__.py
+++ diffusion/__init__.py
@@ -1,11 +1,14 @@
 """Diffusion Utilities"""
 
 from .noise_schedules import linear_beta, quadratic_beta, cosine_beta
-from .diffuser import Diffuser
+from .diffuser import Diffuser, extract
+from .sampling import sample_probabilistic
 
 __all__ = [
     "linear_beta",
     "quadratic_beta",
     "cosine_beta",
     "Diffuser",
+    "extract",
+    "sample_probabilistic",
 ]
